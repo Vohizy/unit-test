7424f4be5bc827211623f12d8ab1d4b8865a6d17
vohizy <hei.vohizy@gmail.com>
2023-02-15 12:05:21 +0300

    feat: first test cypress

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,13 @@
+describe('First test in cypress', () => {
+  it('cypress runs correctly', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains("Kitchen Sink")
+    cy.contains("children").click();
+    cy.url().should('contains' , 'traversal')/* dans le page de url de children que j'ai click contient le mots traversal */
+  })
+  xit('app runs correctly', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#add-todo').type('lorem')
+  })
+
+})
